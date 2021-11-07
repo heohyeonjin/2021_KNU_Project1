d3203d18fdd1b7f5d9cf7d8aa2a750cae8d2aa2b
idoburnish <doyun7433@naver.com>
2021-11-07 23:59:03 +0900

    add corp_new layout

---
+++ Web/nav/nav.php
@@ -0,0 +1,37 @@
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">빠른답변</a>
+  <a class="logout nav-link px-4" href="#" style="color:white;">Logout</a> <!-- logout 페이지로 변환 -->
+</header>
+
+<div class="container-fluid"> <!-- 대시보드 -->
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+      <img src="../resources/logo.png" alt="" width="210px">  
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-0 text-muted">
+        <span>관리</span>      
+      </h6>
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column"> <!--nav-tabs -->
+          <li class="nav-item mb-3">
+            <a class="nav-link" aria-current="page" href="#"> <!-- active-->
+              <img src="../resources/corporation.svg" alt="" width="25px">  
+              기업관리
+            </a>
+          </li>
+          <li class="nav-item mb-3">
+            <a class="nav-link" href="#">
+              <img src="../resources/counselor.svg" alt="" width="25px">  
+              상담원관리
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+            <img src="../resources/client.svg" alt="" width="25px">  
+              고객관리
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</div>
