d57476612ecf458db7904a1c627fc58b369bdcd3
idoburnish <doyun7433@naver.com>
2021-11-08 19:57:57 +0900

    apply css changes to php

--- Web/auth/auth.php
+++ Web/auth/auth.php
@@ -7,12 +7,12 @@
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel ="stylesheet" href="auth.css">
+    <link rel ="stylesheet" href="auth.css?after">
     <title>login</title>
 </head>
 <body>
     <div class="login">
-        <div class="login_left" style="background-color: #343a40;">
+        <div class="login_left" >
             <br><br>
             <img src="../resources/client.svg" alt="" width="210px"> 
             <br><br>
