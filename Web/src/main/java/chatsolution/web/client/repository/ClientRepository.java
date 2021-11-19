f6bdd12ae4e27fe53ae816f240bc37c3192b687d
idoburnish <doyun7433@naver.com>
2021-11-19 19:47:35 +0900

    implemnet client signUp

---
+++ Web/src/main/java/chatsolution/web/client/repository/ClientRepository.java
@@ -0,0 +1,8 @@
+package chatsolution.web.client.repository;
+
+import chatsolution.web.client.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByClientEmail(String email);
+}
