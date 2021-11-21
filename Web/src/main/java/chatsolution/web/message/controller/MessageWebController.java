e8d756fa4547dca5c1ddc645f479ce49abf89671
idoburnish <doyun7433@naver.com>
2021-11-21 22:47:41 +0900

    implement counseling roomList

--- Web/src/main/java/chatsolution/web/message/controller/MessageWebController.java
+++ Web/src/main/java/chatsolution/web/message/controller/MessageWebController.java
@@ -1,6 +1,8 @@
 package chatsolution.web.message.controller;
 
+import chatsolution.web.message.dto.MessageListDto;
 import chatsolution.web.message.dto.NewMessageDto;
+import chatsolution.web.message.dto.RoomListDto;
 import chatsolution.web.message.service.MessageWebService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -18,21 +21,31 @@ public class MessageWebController {
 
     private final MessageWebService messageWebService;
 
+    // 채팅방 목록
     @GetMapping
     public String roomList(Model model, HttpServletRequest request) {
+
         Long enter = (Long)request.getSession().getAttribute("counNo");
+        log.info("로그인한 상담원 일련번호: " + enter);
+
+        List<RoomListDto> rooms = messageWebService.roomList(enter);
+        model.addAttribute("rooms", rooms);
         return "chat/chat_list";
     }
 
-//    @GetMapping
-//    public String chatList(Model model){
-//        List<MessageListDto> messages = messageService.msgList();
-//        model.addAttribute("messages", messages);
-//        return "chat/chat_room";
-//    }
+    // 채팅방 입장
+    @GetMapping("/{roomNo}")
+    public String enterRoom(@PathVariable Long roomNo, Model model, HttpServletRequest request) {
 
-    @PostMapping("/send")
-    public @ResponseBody String msgSend(NewMessageDto newMessageDto) {
+        Long enter = (Long)request.getSession().getAttribute("counNo");
+        List<MessageListDto> messages = messageWebService.msgList(enter, roomNo);
+
+        model.addAttribute("messages", messages);
+        return "chat/chat_room";
+    }
+
+    @PostMapping("/{roomNo}/send")
+    public @ResponseBody String msgSend(@PathVariable Long roomNo, NewMessageDto newMessageDto) {
         log.info("전달받은 메세지: " + newMessageDto.getMsg());
         messageWebService.saveMsg(newMessageDto);
         String returnValue = "true";
